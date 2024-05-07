c43278d8a197e18f29e9a7f533d2a3d6663e9188
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-05-07 15:51:45 +1200

    added player name as class variable

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,27 +6,31 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   public static int roundNumber = 1;
+  public static String playerName;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]); // prints options[0] as the player name
+    Game.playerName = options[0];
   }
 
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
-    roundNumber++; // increment round number by 1
 
-    MessageCli.ASK_INPUT.printMessage(); // prints "Give <fingers> and press enter"
+    MessageCli.ASK_INPUT.printMessage();
     String inputFingers = Utils.scanner.nextLine(); // reads input from the user
 
-    // if the input is not a number between 0 and 5, print "Error! Invalid input, you should give
-    // one integer number between 0 and 5 (inclusive), please try again"
-    while (!inputFingers.matches("[0-5]")) {
+    // if the input is not a number between 0 and 5, print error message and prompt again
+    int InputFingersInt = Integer.parseInt(inputFingers);
+
+    while (InputFingersInt < 0 || InputFingersInt > 5) {
       MessageCli.INVALID_INPUT.printMessage();
+      MessageCli.ASK_INPUT.printMessage();
       inputFingers = Utils.scanner.nextLine();
+      InputFingersInt = Integer.parseInt(inputFingers);
     }
 
     MessageCli.PRINT_INFO_HAND.printMessage(
-        "1", inputFingers); // prints "Player 1: fingers: <input>"
+        playerName, inputFingers); // prints the player name and the number of fingers
   }
 
   public void endGame() {}
