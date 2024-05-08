100e0ee6919025f0b17528432456e61dc6d5d012
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-05-08 15:08:27 +1200

    created medium and hard bot classes

---
+++ src/main/java/nz/ac/auckland/se281/Hard.java
@@ -0,0 +1,7 @@
+package nz.ac.auckland.se281;
+
+public class Hard implements Bot {
+  public void returnName() {
+    System.out.println("Hard Bot");
+  }
+}
---
+++ src/main/java/nz/ac/auckland/se281/Medium.java
@@ -0,0 +1,7 @@
+package nz.ac.auckland.se281;
+
+public class Medium implements Bot {
+  public void returnName() {
+    System.out.println("Medium Bot");
+  }
+}
