43fbb8b52ab6147f5c1507b4b1b6ac4f760509d2
emmanuelaraba <emmanuelaraba77@gmail.com>
2024-05-10 09:27:15 +1200

    implemented logic to fnid the winner of the round

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -8,24 +8,43 @@ public class Game {
   private int roundNumber = 1;
   private String playerName;
   private Bot bot; // Bot object
-  private String choice; // choice of the player (EVEN or ODD)
+  private Choice playerChoice; // choice of the player (EVEN or ODD)
+  private String inputFingers; // input from the player
+  final String computerName = "HAL 9000"; // name of the computer bot "HAL 9000
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]); // prints options[0] as the player name
     this.playerName = options[0];
     this.bot = BotFactory.makeBot(difficulty); // creates a new bot
-    this.choice = choice.toString(); // sets the choice
+    this.playerChoice = choice; // sets the choice
 
     roundNumber = 1; // resets the round number
   }
 
-  public void getWinner(int botMove) {}
+  public void getWinner(int botMove) {
+    int inputFingersInt = Integer.parseInt(inputFingers);
+    int sum = inputFingersInt + botMove;
+
+    if (sum % 2 == 0) {
+      if (playerChoice == Choice.EVEN) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(sum), "EVEN", playerName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(sum), "ODD", computerName);
+      }
+    } else {
+      if (playerChoice == Choice.ODD) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(sum), "ODD", playerName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(Integer.toString(sum), "EVEN", computerName);
+      }
+    }
+  }
 
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(roundNumber));
 
     MessageCli.ASK_INPUT.printMessage();
-    String inputFingers = Utils.scanner.nextLine(); // reads input from the user
+    this.inputFingers = Utils.scanner.nextLine(); // reads input from the user
 
     // if the input is not a number between 0 and 5, print error message and prompt again
     int InputFingersInt = Integer.parseInt(inputFingers);
--- src/main/java/nz/ac/auckland/se281/Strategy.java
+++ src/main/java/nz/ac/auckland/se281/Strategy.java
@@ -2,6 +2,9 @@ package nz.ac.auckland.se281;
 
 /** Strategy */
 public interface Strategy {
-
+  /*
+  *
+  @returns the
+  */
   public int chooseMove(); // choosing the move to be made
 }
